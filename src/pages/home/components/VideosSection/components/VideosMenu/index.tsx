e765166e28b634c7b702312830d6286f63163c3d
unknown <lc.bonini13@gmail.com>
2023-06-27 16:07:51 -0300

    Feat: Filtro pelos botões

--- src/pages/home/components/VideosSection/components/VideosMenu/index.tsx
+++ src/pages/home/components/VideosSection/components/VideosMenu/index.tsx
@@ -4,17 +4,21 @@ import Button from '@components/Button'
 // Styled Components
 import DropDown from '@components/DropDown'
 import { AXIOS } from '@libs'
-import { IFilterOptions } from '@types'
+import { IFilterOptions, VideosMenuProps } from '@types'
 import { useCallback, useEffect, useState } from 'react'
 import VideosMenuCardLabel from './styles/VideosMenuCardLabel.styles'
 import VideosMenuContainer from './styles/VideosMenuContainer.styles'
 import VideosMenuContent from './styles/VideosMenuContent.styles'
 import VideosMenuDropDown from './styles/VideosMenuDropDown.styles'
 
-const VideosMenu = () => {
+const VideosMenu = ({ onChangeFilter }: VideosMenuProps) => {
   const [dropDownOptions, setDropDownOptions] = useState<Array<IFilterOptions>>(
     [],
   )
+  const [filter, setFilter] = useState<IFilterOptions>({
+    id: 1,
+    name: 'agency',
+  })
 
   const getDropDownFilters = useCallback(async () => {
     try {
@@ -29,14 +33,43 @@ const VideosMenu = () => {
     getDropDownFilters()
   }, [getDropDownFilters])
 
+  useEffect(() => {
+    onChangeFilter(filter)
+  }, [filter])
+
   return (
     <VideosMenuContainer>
       <VideosMenuContent>
-        <Button>Agências</Button>
-        <Button>Chatbot</Button>
-        <Button>Marketing Digital</Button>
-        <Button>Geração de Leads</Button>
-        <Button>Mídia Paga</Button>
+        <Button
+          isActive={filter.id === 1}
+          onClick={() => setFilter({ id: 1, name: 'agency' })}
+        >
+          Agências
+        </Button>
+        <Button
+          isActive={filter.id === 2}
+          onClick={() => setFilter({ id: 2, name: 'chatbot' })}
+        >
+          Chatbot
+        </Button>
+        <Button
+          isActive={filter.id === 3}
+          onClick={() => setFilter({ id: 3, name: 'digitalMarketing' })}
+        >
+          Marketing Digital
+        </Button>
+        <Button
+          isActive={filter.id === 4}
+          onClick={() => setFilter({ id: 4, name: 'leadsGeneration' })}
+        >
+          Geração de Leads
+        </Button>
+        <Button
+          isActive={filter.id === 5}
+          onClick={() => setFilter({ id: 5, name: 'paidMedia' })}
+        >
+          Mídia Paga
+        </Button>
         <VideosMenuDropDown>
           <VideosMenuCardLabel>Ordenar por</VideosMenuCardLabel>
           <DropDown options={dropDownOptions} />
