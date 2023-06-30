8c1350f63db8681c147b7c4bd4145ba38d4bae02
unknown <lc.bonini13@gmail.com>
2023-06-29 22:09:41 -0300

    refactor: Responsividade do Card

--- src/components/Card/styles/CardTitle.styles.tsx
+++ src/components/Card/styles/CardTitle.styles.tsx
@@ -3,5 +3,9 @@ import { styled } from 'styled-components'
 const CardTitle = styled.p`
   font-family: var(--plus-jakarta-sans-bold);
   padding: 2rem;
+  @media screen and (max-width: 425px) {
+    /* Estilos para dispositivos móveis grandes */
+    font-size: 1.4rem;
+  }
 `
 export default CardTitle
