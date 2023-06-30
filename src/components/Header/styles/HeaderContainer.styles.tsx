9cea53f8e2d6fb376da2488b6de90c3960c3181d
unknown <lc.bonini13@gmail.com>
2023-06-29 22:12:23 -0300

    refactor: Responsividade do Header

--- src/components/Header/styles/HeaderContainer.styles.tsx
+++ src/components/Header/styles/HeaderContainer.styles.tsx
@@ -5,6 +5,11 @@ const HeaderContainer = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  @media screen and (max-width: 425px) {
+    /* Estilos para dispositivos móveis grandes */
+    height: 5rem;
+  }
 `
 
 export default HeaderContainer
